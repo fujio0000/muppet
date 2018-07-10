94a147dfc3f7ac402eff39d5b23f3092c89443c1
abetomo <abe@enzou.tokyo>
2018-07-10 10:14:10 +0900

    Fix to use `const` instead of `var`

--- muppet.js
+++ muppet.js
@@ -5,10 +5,10 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  var width = Number(process.argv[2]);
-  var height = Number(process.argv[3]);
-  var filepath = String(process.argv[4]);
-  var url = String(process.argv[5]);
+  const width = Number(process.argv[2]);
+  const height = Number(process.argv[3]);
+  const filepath = String(process.argv[4]);
+  const url = String(process.argv[5]);
   await page.goto(url);
   await page.setViewport({
     width: width,
